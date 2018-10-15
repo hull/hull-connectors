81fa7771441e2fe11a6ea41927b092d12271471c
Michal Raczka <michal@raczka.me>
2018-10-15 14:12:57 +0200

    Fix hull-client test

--- packages/hull-client/test/integration/retry-test.js
+++ packages/hull-client/test/integration/retry-test.js
@@ -7,30 +7,29 @@ describe("client retrying", function test() {
   let minihull, client;
   beforeEach(() => {
     minihull = new Minihull();
-    minihull.listen(8000);
     client = new Hull({
       organization: "localhost:8000",
       id: "111111111111111111111111",
       secret: "rocks",
       protocol: "http"
     });
+    return minihull.listen(8000);
   });
 
   afterEach(() => {
-    minihull.close();
+    return minihull.close();
   });
 
-  it("should retry 2 times if get 503 response, then reject", (done) => {
+  it("should retry 2 times if get 503 response, then reject", () => {
     const stub = minihull.stubApp("/api/v1/testing")
       .callsFake((req, res) => {
         res.status(503).end("error 503");
       });
 
-    client.get("/testing")
+    return client.get("/testing")
       .catch(err => {
         expect(err).to.not.be.undefined;
         expect(stub.callCount).to.be.eql(3);
-        done();
       });
   });
 
@@ -48,7 +47,7 @@ describe("client retrying", function test() {
       });
   });
 
-  it("should retry first 503 response, then resolve", (done) => {
+  it("should retry first 503 response, then resolve", () => {
     const stub = minihull.stubApp("/api/v1/testing")
       .onFirstCall()
       .callsFake((req, res) => {
@@ -59,27 +58,24 @@ describe("client retrying", function test() {
         res.end("ok")
       });
 
-    client.get("/testing")
+    return client.get("/testing")
       .then(() => {
         expect(stub.callCount).to.be.eql(2);
-        minihull.close();
-        done();
       });
   });
 
-  it("shoud retry 2 times on timeout, then reject", (done) => {
+  it("shoud retry 2 times on timeout, then reject", () => {
     const stub = minihull.stubApp("/api/v1/testing")
       .callsFake((req, res) => {});
 
-    client.get("/testing", {}, { timeout: 20, retry: 10 })
+    return client.get("/testing", {}, { timeout: 20, retry: 10 })
       .catch(err => {
         expect(err.message).to.equal("Timeout of 20ms exceeded");
         expect(stub.callCount).to.be.eql(3);
-        done();
       });
   });
 
-  it("shoud retry first timeout, then resolve", (done) => {
+  it("shoud retry first timeout, then resolve", () => {
     const stub = minihull.stubApp("/api/v1/testing")
       .onFirstCall()
       .callsFake((req, res) => {
@@ -89,10 +85,9 @@ describe("client retrying", function test() {
         res.end("ok")
       });
 
-    client.get("/testing", {}, { timeout: 20, retry: 10 })
+    return client.get("/testing", {}, { timeout: 20, retry: 10 })
       .then(() => {
         expect(stub.callCount).to.be.eql(2);
-        done();
       });
   });
 });
