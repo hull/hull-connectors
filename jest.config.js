b5b4ff0e9593d8d1137468c2b0c4f3ea814dce32
Michal Raczka <michal@raczka.me>
2018-10-18 10:56:56 +0200

    Enable the tests

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   testMatch: [
     "<rootDir>/packages/connectors/hull-typeform/test/**/*.(test|spec).js",
-    // "<rootDir>/packages/connectors/hull-outreach/test/**/*.(test|spec).js"
+    "<rootDir>/packages/connectors/hull-outreach/test/**/*.(test|spec).js"
   ],
   collectCoverageFrom: [
     "<rootDir>/packages/connectors/hull-typeform/server/**/*.js",
-    // "<rootDir>/packages/connectors/hull-outreach/server/**/*.js"
+    "<rootDir>/packages/connectors/hull-outreach/server/**/*.js"
   ],
   collectCoverage: true,
   coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
