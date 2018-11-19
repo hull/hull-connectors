50b32e459b5298f57ee9413527738d95021d2578
Michal Raczka <michal@raczka.me>
2018-11-19 14:52:47 +0100

    Hubspot migration script

---
+++ packages/connectors/hull-hubspot/migrations/outgoing-user-attributes.js
@@ -0,0 +1,45 @@
+function migrateConnector(connector) {
+  const outgoingUserAttributes = connector.private_settings.sync_fields_to_hubspot.map(
+    entry => {
+      return {
+        hull: entry.hull,
+        service: entry.name,
+        overwrite: entry.overwrite
+      };
+    }
+  );
+  const incomingUserAttributes = connector.private_settings.sync_fields_to_hull.map(
+    entry => {
+      return {
+        hull: entry.hull,
+        service: entry.name
+      };
+    }
+  );
+
+  const outgoingAccountAttributes = connector.private_settings.outgoing_account_attributes.map(
+    entry => {
+      return {
+        hull: entry.hull,
+        service: entry.hubspot
+      };
+    }
+  );
+  const incomingAccountAttributes = connector.private_settings.incoming_account_attributes.map(
+    entry => {
+      return {
+        hull: entry.hull,
+        service: entry.hubspot
+      };
+    }
+  );
+
+  return {
+    outgoing_user_attributes: outgoingUserAttributes,
+    incoming_user_attributes: incomingUserAttributes,
+    outgoing_account_attributes: outgoingAccountAttributes,
+    incoming_account_attributes: incomingAccountAttributes
+  };
+}
+
+module.exports = migrateConnector;
