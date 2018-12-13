9267d33c1f9764da33b465a6034ae5825bbab97a
Tim Liu <tim@hull.io>
2018-12-13 19:19:40 +0100

    added user batch test

--- packages/connectors/hull-outreach/test/integration/scenarios/fetch-all-no-link.test.js
+++ packages/connectors/hull-outreach/test/integration/scenarios/fetch-all-no-link.test.js
@@ -9,7 +9,7 @@ process.env.CLIENT_SECRET = "1234";
 const testScenario = require("hull-connector-framework/src/test-scenario");
 const connectorServer = require("../../../server/server");
 
-test("fetch all accounts and prospects from outreach", () => {
+test("fetch all accounts and prospects from outreach no user/account linking", () => {
   return testScenario({ connectorServer }, ({ handlers, nock, expect }) => {
     return {
       handlerType: handlers.scheduleHandler,
