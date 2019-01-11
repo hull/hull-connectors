e8ef385a0a19c5aebe603dd14405d281cb034ea9
Tim Liu <tim@hull.io>
2019-01-11 15:56:13 -0500

    added instructions if unauthorized, also added test

--- packages/connectors/hull-hubspot/server/actions/status-check.js
+++ packages/connectors/hull-hubspot/server/actions/status-check.js
@@ -5,13 +5,33 @@ const _ = require("lodash");
 
 const SyncAgent = require("../lib/sync-agent");
 
+function getMessageFromError(err): string {
+
+  console.log(err.message);
+  if (err.message && typeof err.message === 'string'
+      && err.message.indexOf("Failed to refresh access token") === 0) {
+    return "Unauthorized response from Hubspot. Please reauthenticate with Hubspot by clicking the \"Credentials and Actions\" button in the upper right hand section of the connector settings.  Then either click \"Continue to Hubspot\" or \"Start over\"";
+  }
+
+  // return either the msg or message parameter from err
+  return `Could not get response from Hubspot due to error ${_.get(
+    err,
+    "msg",
+    _.get(err, "message", "")
+  )}`;
+}
+
 function statusCheckAction(ctx: HullContext) {
   const { connector = {}, client = {} } = ctx;
   const messages = [];
   let status = "ok";
   const pushMessage = message => {
     status = "error";
-    messages.push(message);
+
+    // make sure the message isn't already in the list...
+    if (messages.indexOf(message) < 0) {
+      messages.push(message);
+    }
   };
   const promises = [];
 
@@ -52,13 +72,7 @@ function statusCheckAction(ctx: HullContext) {
           }
         })
         .catch(err => {
-          pushMessage(
-            `Could not get response from Hubspot due to error ${_.get(
-              err,
-              "msg",
-              _.get(err, "message", "")
-            )}`
-          );
+          pushMessage(getMessageFromError(err));
         })
     );
     promises.push(
@@ -84,13 +98,7 @@ function statusCheckAction(ctx: HullContext) {
           }
         })
         .catch(err => {
-          pushMessage(
-            `Could not get response from Hubspot due to error ${_.get(
-              err,
-              "msg",
-              _.get(err, "message", "")
-            )}`
-          );
+          pushMessage(getMessageFromError(err));
         })
     );
   }
