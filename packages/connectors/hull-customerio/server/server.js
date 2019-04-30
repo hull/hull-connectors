7c74a591e517c37eadc477402dda5198d2167f00
Tim Liu <tim@hull.io>
2019-04-30 10:04:12 -0400

    fixed incoming webhooks handler

--- packages/connectors/hull-customerio/server/server.js
+++ packages/connectors/hull-customerio/server/server.js
@@ -2,23 +2,56 @@
 import type { $Application, $Response } from "express";
 import type { HullRequest, HullUserUpdateMessage } from "hull";
 
-const { notificationHandler, batchHandler } = require("hull/src/handlers");
+const { notificationHandler, batchHandler, htmlHandler } = require("hull/src/handlers");
+
 const bodyParser = require("body-parser");
 
 const { webhookHandler, statusCheck, updateUser } = require("./actions");
 const { encrypt } = require("./lib/crypto");
 const { middleware } = require("./lib/crypto");
 
+const {
+  credentialsFromQueryMiddleware,
+  fullContextFetchMiddleware,
+  timeoutMiddleware,
+  haltOnTimedoutMiddleware,
+  clientMiddleware,
+  instrumentationContextMiddleware,
+  instrumentationTransientErrorMiddleware
+} = require("hull/src/middlewares");
+
 function server(app: $Application, { hostSecret }: Object): $Application {
-  app.get("/admin.html", (req: HullRequest, res: $Response) => {
-    const token = encrypt(req.hull.config, hostSecret);
-    res.render("admin.html", { hostname: req.hostname, token });
-  });
+
+  app.get("/admin.html", htmlHandler((ctx) => {
+    const token = encrypt(ctx.clientCredentials, ctx.connectorConfig.hostSecret);
+    return  Promise.resolve({
+      pageLocation: "admin.html",
+      data: { hostname: ctx.hostname, token }
+    });
+  }));
+
+  // app.get("/admin.html", (req: HullRequest, res: $Response) => {
+  //   const token = encrypt(req.hull.config, hostSecret);
+  //   res.render("admin.html", { hostname: req.hostname, token });
+  // });
+
+  // app.all(
+  //   "/webhook",
+  //   bodyParser.json(),
+  //   middleware(hostSecret),
+  //   webhookHandler
+  // );
 
   app.all(
     "/webhook",
     bodyParser.json(),
     middleware(hostSecret),
+    timeoutMiddleware(),
+    clientMiddleware(),
+    haltOnTimedoutMiddleware(),
+    instrumentationContextMiddleware(),
+    fullContextFetchMiddleware({ requestName: "action" }),
+    haltOnTimedoutMiddleware(),
     webhookHandler
   );
 
