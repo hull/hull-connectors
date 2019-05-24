a75a7a52eac36a56ddc300ba72ed4763021e9279
Romain Dardour <romain@hull.io>
2019-05-24 19:49:53 +0200

    rsync packages

--- scripts/build-connectors.sh
+++ scripts/build-connectors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # this scripts builds a webpack project
-babel packages -d dist --delete-dir-on-start --verbose --copy-files --ignore "packages/connectors/*/src/**","packages/connectors/*/test/**","packages/hull-vm/src/**"
+babel packages -d dist --delete-dir-on-start --verbose --copy-files --ignore "packages/connectors/*/src/**","packages/connectors/*/test/**","packages/hull-vm/src/**" && rsync -rl packages/ dist/ --ignore-existing
 webpack --config webpack;
