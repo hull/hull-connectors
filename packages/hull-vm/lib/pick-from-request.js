a7e99c4af085f88614ec409313158542eaf2c783
Romain Dardour <romain@hull.io>
2019-06-02 21:55:28 +0200

    Make Incoming Webhooks work well

--- packages/hull-vm/lib/pick-from-request.js
+++ packages/hull-vm/lib/pick-from-request.js
@@ -3,22 +3,11 @@ import _ from "lodash";
 import type { HullIncomingHandlerMessage } from "hull";
 import type { Payload } from "../types";
 
-const pickValuesFromRequest = ({
-  body,
-  headers,
-  cookies,
-  ip,
-  method,
-  params,
-  query
-}: HullIncomingHandlerMessage): Payload => ({
-  body,
-  cookies,
-  ip,
-  method,
-  params,
-  query,
-  headers: _.omit(headers, [
+const pickValuesFromRequest = (
+  payload: HullIncomingHandlerMessage
+): Payload => ({
+  ...payload,
+  headers: _.omit(payload.headers, [
     "x-forwarded-for",
     "x-forwarded-proto",
     "x-newrelic-id",
