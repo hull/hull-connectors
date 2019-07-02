984a4fc1be531f03808294727ebb6d1c0068bdf3
Louis Jahn <louis@hull.io>
2019-07-02 14:55:42 +0200

    Fixed eslint "import/no-unresolved" rule (for ci tests)

--- packages/hull/src/index.js
+++ packages/hull/src/index.js
@@ -3,11 +3,12 @@
 /* :: export type * from "./types"; */
 
 import HullClient from "hull-client";
-import type { HullConnectorConfig } from "./types";
+import type { HullConnectorConfig } from "./types/connector";
 
 const Worker = require("./connector/worker");
 const ConnectorClass = require("./connector/hull-connector");
 
+export type { HullConnectorConfig };
 export { default as Client } from "hull-client";
 export class Connector extends ConnectorClass {
   config: HullConnectorConfig;
