c04deb9fde7665eff16bf5a3447cb7315c36e403
giovanni carnel <giovanni@hull.io>
2019-07-23 12:26:28 +0200

    mailchimp status

--- packages/connectors/hull-mailchimp/server/actions/status.js
+++ packages/connectors/hull-mailchimp/server/actions/status.js
@@ -28,7 +28,7 @@ async function statusAction(ctx: HullContext): HullStatusResponse {
 
     if (!shipApp.syncAgent.isListConfigured()) {
       return {
-        status: "ok",
+        status: "setupRequired",
         messages: ["Please select a Mailchimp list to synch with"]
       };
     }
