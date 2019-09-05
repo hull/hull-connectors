506d9bb92110fae502a89f727ab045c2a96d50da
Andy <andy@Andys-Air.localdomain>
2019-09-05 15:27:27 -0400

    linting

--- packages/connectors/hull-hubspot/server/handlers/incoming-webhooks-handler.js
+++ packages/connectors/hull-hubspot/server/handlers/incoming-webhooks-handler.js
@@ -2,14 +2,9 @@
 
 import type {
   HullContext,
-  // HullResponse,
   HullIncomingHandlerMessage,
   HullExternalResponse
 } from "hull";
-
-
-import ingest from "../lib/ingest";
-import type { Event } from "../types";
 
 const HubspotPurpleFusionRouter = require("../lib/hubspot-purple-fusion-router");
 
@@ -17,12 +12,6 @@ export default async (
   ctx: HullContext,
   message: HullIncomingHandlerMessage
 ): HullExternalResponse => {
-  /*
-  const hubspotEntityId = _.get(message.body[0], "objectId", "");
-  const hubspotEventOccurredAt = _.get(message.body[0], "occurredAt", ""); // store on hubspot/deleted_at
-  const hubspotChangeFlag = _.get(message.body[0], "changeFlag", ""); // DELETED
-   */
-
   const route = "incomingWebhooksHandler";
 
   const router = new HubspotPurpleFusionRouter(route);
