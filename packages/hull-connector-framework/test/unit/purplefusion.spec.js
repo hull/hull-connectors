190b2e14b403d0b32ef916958ba21ddaf671bb56
Tim Liu <tim@hull.io>
2019-09-05 16:56:55 -0400

    updated account tests to be testing the right thing

--- packages/hull-connector-framework/test/unit/purplefusion.spec.js
+++ packages/hull-connector-framework/test/unit/purplefusion.spec.js
@@ -156,123 +156,72 @@ describe("Outgoing Account Segment Filtering Tests", () => {
   });
 
   it("outgoing account all segments account attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "ALL" ], outgoing_user_attributes: [ {hull: "account.domain", service: "field" }] }});
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "ALL" ], outgoing_account_attributes: [ {hull: "domain", service: "field" }] }});
     expect(toSendMessage(context,
-      "user", { segments: [], user: { email: "someuser@gmail.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(true)
+      "account", { account_segments: [], account: { domain: "somedomain.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(true)
   });
 
-  it("outgoing user segment account attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], outgoing_user_attributes: [ {hull: "account.domain", service: "field" }] }});
+  it("outgoing account segment account attribute change", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ], outgoing_account_attributes: [ {hull: "domain", service: "field" }] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(true)
+      "account", { account_segments: [ { id: "1234" } ], account: { domain: "somedomain.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(true)
   });
 
-  it("outgoing user all segments user attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "ALL" ], outgoing_user_attributes: [ {hull: "closeio/title", service: "field" }] }});
-    expect(toSendMessage(context,
-      "user", { segments: [], user: { email: "someuser@gmail.com"}, changes: { user: { "closeio/title": [null, "sometitle"]}} })).toEqual(true)
-  });
 
-  it("outgoing user segment user attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], outgoing_user_attributes: [ {hull: "closeio/title", service: "field" }] }});
+  it("outgoing account entered segment", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { user: { "closeio/title": [null, "sometitle"]}} })).toEqual(true)
-  });
-
-  it("outgoing user entered segment", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
-    expect(toSendMessage(context,
-      "user", { segments: [ ], user: { email: "someuser@gmail.com"}, changes: { segments: { entered: [ { id: "1234" }]}} })).toEqual(true)
+      "account", { account_segments: [ ], account: { domain: "somedomain.com"}, changes: { account_segments: { entered: [ { id: "1234" }]}} })).toEqual(true)
   });
 
 
   // Test for sendOnAnySegmentChanges for cases that we're sending segments by default and we need to send on any change
-  it("outgoing user send on sendOnAnySegmentChanges option(entered), for synching hull_segments", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
+  it("outgoing account send on sendOnAnySegmentChanges option(entered), for synching hull_segments", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user",
-      { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { segments: { entered: [ { id: "1234" }]}} },
+      "account",
+      { account_segments: [ { id: "1234" } ], account: { domain: "somedomain.com"}, changes: { account_segments: { entered: [ { id: "5678" }]}} },
       { sendOnAnySegmentChanges: true })).toEqual(true)
   });
 
-  it("outgoing user send on sendOnAnySegmentChanges option(left), for synching hull_segments", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
+  it("outgoing account send on sendOnAnySegmentChanges option(left), for synching hull_segments", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { segments: { left: [ { id: "1234" }]}} },
+      "account", { account_segments: [ { id: "1234" } ], account: { domain: "somedomain.com"}, changes: { account_segments: { left: [ { id: "5678" }]}} },
       { sendOnAnySegmentChanges: true })).toEqual(true)
   });
 
-  it("outgoing user send on sendOnAnySegmentChanges option, no changed segments", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
+  it("outgoing account send on sendOnAnySegmentChanges option, no changed segments", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { domain: [null, "somedomain.com"]}} },
+      "account", { account_segments: [ { id: "1234" } ], account: { domain: "somedomain.com"}, changes: { account: { domain: [null, "somedomain.com"]}} },
       { sendOnAnySegmentChanges: true })).toEqual(false)
-  });
-
-  //testing on account link changes (different identifiers can be an account link change)
-  it("outgoing user account link changes", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], link_users_in_service: true }});
-    expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { id: [null, "1234"]}} },
-      { sendOnAnySegmentChanges: true })).toEqual(true)
-  });
-
-  it("outgoing user service account id changes", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], link_users_in_service: true }});
-    expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { "hubspot/id": [null, "1234"]}} },
-      { serviceName: "hubspot" })).toEqual(true)
-  });
-
-  // must specify a service name to know what attribute to check on the account
-  it("outgoing user service account id changes, but no serviceName specified", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], link_users_in_service: true }});
-    expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { "hubspot/id": [null, "1234"]}} })).toEqual(false)
-  });
-
-  it("outgoing user specified associated account id changes at account level", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], outgoing_user_associated_account_id: "account.salesforce/someid" }});
-    expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { "salesforce/someid": ["asdf", "1234"]}} })).toEqual(true)
-  });
-
-  it("outgoing user specified associated account id changes at user level", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], outgoing_user_associated_account_id: "salesforce/someid" }});
-    expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { user: { "salesforce/someid": ["asdf", "1234"]}} })).toEqual(true)
   });
 
 
   // negative tests
-  it("outgoing user all segments no attribute changes", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "ALL" ] }});
+  it("outgoing account all segments no attribute changes", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "ALL" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [], user: { email: "someuser@gmail.com"} })).toEqual(false)
+      "account", { account_segments: [ ], account: { domain: "somedomain.com"} })).toEqual(false)
   });
 
-  it("outgoing user segment no attribute changes", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
+  it("outgoing account segment no attribute changes", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"} })).toEqual(false)
+      "account", { account_segments: [ { id: "1234" } ], account: { domain: "somedomain.com"} })).toEqual(false)
   });
 
-  it("outgoing user all segments different account attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "ALL" ], outgoing_user_attributes: [ {hull: "account.closeio/status", service: "field" }] }});
+  it("outgoing  all segments different attribute change", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "ALL" ], outgoing_account_attributes: [ {hull: "closeio/status", service: "field" }] }});
     expect(toSendMessage(context,
-      "user", { segments: [], user: { email: "someuser@gmail.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(false)
+      "account", { account_segments: [], account: { domain: "somedomain.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(false)
   });
 
-  it("outgoing user segment account attribute change", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ], outgoing_user_attributes: [ {hull: "account.closeio/status", service: "field" }] }});
+  it("outgoing account entered not synchronized segment", () => {
+    const context = new ContextMock({ private_settings: { synchronized_account_segments: [ "1234" ] }});
     expect(toSendMessage(context,
-      "user", { segments: [ { id: "1234" } ], user: { email: "someuser@gmail.com"}, changes: { account: { domain: [null, "somedomain.com"]}} })).toEqual(false)
-  });
-
-  it("outgoing user entered not synchronized segment", () => {
-    const context = new ContextMock({ private_settings: { synchronized_user_segments: [ "1234" ] }});
-    expect(toSendMessage(context,
-      "user", { segments: [ ], user: { email: "someuser@gmail.com"}, changes: { segments: { entered: [ { id: "567" }]}} })).toEqual(false)
+      "account", { account_segments: [ "1234" ], account: { domain: "somedomain.com"}, changes: { segments: { entered: [ { id: "567" }]}} })).toEqual(false)
   });
 
 
