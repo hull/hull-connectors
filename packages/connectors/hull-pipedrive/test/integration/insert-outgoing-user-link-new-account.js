13aeb87bdac2d49454e9b069c8d7c5f7e1e89c7a
Andy Hull <andy@hull.io>
2019-10-17 15:02:45 -0400

    fix tests

--- packages/connectors/hull-pipedrive/test/integration/insert-outgoing-user-link-new-account.js
+++ packages/connectors/hull-pipedrive/test/integration/insert-outgoing-user-link-new-account.js
@@ -9,7 +9,164 @@ import connectorConfig from "../../server/config";
 
 it("Insert Single User To Pipedrive Link New Account", () => {
   return testScenario({ connectorConfig }, ({ handlers, nock, expect }) => {
-    const updateMessages = require("./fixtures/notifier-payloads/new-single-user-account");
+    const updateMessages = {
+      "notification_id": "38108659-4d7b-46cc-861b-3da772d1fec2",
+      "configuration": {
+        "id": "123456789012345678901234",
+        "organization": "c5011027.hullbeta.io",
+        "secret": "shhhh"
+      },
+      "connector": {
+        "description": "some",
+        "tags": [],
+        "source_url": "https://dev-hull-pipedrive.ngrok.io/",
+        "private_settings": {
+          "webhook_id_person": 11111,
+          "webhook_id_org": 11111,
+          "link_users_in_service": true,
+          "user_claims": [
+            {
+              "hull": "email",
+              "service": "email"
+            }
+          ],
+          "account_claims": [
+            {
+              "hull": "domain",
+              "service": "name"
+            }
+          ],
+          "token_expires_in": 7200,
+          "outgoing_user_attributes": [
+            {
+              "hull": "address",
+              "service": "address"
+            },
+            {
+              "hull": "name",
+              "service": "name"
+            }
+          ],
+          "incoming_user_attributes": [
+            {
+              "hull": "pipedrive/description",
+              "service": "4f9ab746d362cdbce1344c14eec9eb2b26ef484b"
+            }
+          ],
+          "token_created_at": 1544104207,
+          "access_token": "1234",
+          "refresh_token": "abcd",
+          "outgoing_account_attributes": [
+            {
+              "hull": "account_name",
+              "service": "name"
+            },
+            {
+              "hull": "account_address",
+              "service": "address"
+            }
+          ],
+          "synchronized_user_segments": [
+            "UserSegment1"
+          ],
+          "synchronized_account_segments": [
+            "AccountSegment1"
+          ],
+          "incoming_account_attributes": [],
+          "created_at": 1544269649
+        },
+        "index": "https://dev-hull-outreach.ngrok.io/",
+        "name": "pipedrive",
+        "extra": {},
+        "settings": {},
+        "type": "ship",
+        "secret": "shhh",
+        "updated_at": "2018-12-09T11:20:32Z",
+        "status": {},
+        "id": "123456789012345678901234",
+        "picture": "",
+        "homepage_url": "",
+        "manifest_url": "",
+        "created_at": "2018-12-06T13:49:58Z"
+      },
+      "channel": "user:update",
+      "messages": [
+        {
+          "changes": {
+            "is_new": false,
+            "account": {
+              "account_address": [
+                "123 Pl",
+                "1 Atlanta Dr"
+              ]
+            },
+            "user": {
+              "address": [
+                "123 Pl",
+                "1234 Hull Pl"
+              ]
+            },
+            "account_segments": {}
+          },
+          "account": {
+            "id": "account_id_2",
+            "account_name": "Apple",
+            "account_address": "1 Atlanta Dr",
+            "domain": "apple.com",
+            "anonymous_ids": []
+          },
+          "user": {
+            "id": "5bd329d5e2bcf3eeaf0000923",
+            "name": "pipedrive_user_1",
+            "email": "pipedrive_user_1@hull.com",
+            "address": "1234 Hull Pl"
+          },
+          "account_segments": [
+            {
+              "id": "AccountSegment1",
+              "name": "AccountSegment1",
+              "updated_at": "2018-12-09T12:05:12Z",
+              "type": "accounts_segment",
+              "created_at": "2018-10-29T14:58:34Z"
+            }
+          ],
+          "segments": [
+            {
+              "id": "UserSegment1",
+              "name": "UserSegment1",
+              "updated_at": "2018-12-09T12:05:12Z",
+              "type": "users_segment",
+              "created_at": "2018-10-29T14:58:34Z"
+            }
+          ],
+          "message_id": "message_1"
+        }
+      ],
+      "accounts_segments": [
+        {
+          "id": "AccountSegment1",
+          "updated_at": "2018-12-09T12:05:12Z",
+          "created_at": "2018-10-29T14:58:34Z",
+          "name": "AccountSegment1",
+          "type": "accounts_segment",
+          "stats": {
+
+          }
+        }
+      ],
+      "segments": [
+        {
+          "id": "UserSegment1",
+          "updated_at": "2018-12-09T12:05:12Z",
+          "created_at": "2018-10-29T14:58:34Z",
+          "name": "UserSegment1",
+          "type": "users_segment",
+          "stats": {
+
+          }
+        }
+      ]
+    };
     return _.assign(updateMessages, {
       handlerType: handlers.notificationHandler,
       handlerUrl: "smart-notifier",
@@ -181,7 +338,7 @@ it("Insert Single User To Pipedrive Link New Account", () => {
           {
             "subject_type": "user",
             "request_id": expect.whatever(),
-            "user_id": "5bd329d5e2bcf3eeaf000099",
+            "user_id": "5bd329d5e2bcf3eeaf0000923",
             "user_email": "pipedrive_user_1@hull.com"
           },
           {
