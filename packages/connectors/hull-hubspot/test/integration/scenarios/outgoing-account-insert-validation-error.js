8e2dbe5da2e85cc136666f23e215682867fc2123
Andy Hull <andy@hull.io>
2019-10-21 12:51:22 -0400

    update tests for logging send account message filtering

--- packages/connectors/hull-hubspot/test/integration/scenarios/outgoing-account-insert-validation-error.js
+++ packages/connectors/hull-hubspot/test/integration/scenarios/outgoing-account-insert-validation-error.js
@@ -4,6 +4,8 @@ import connectorConfig from "../../../server/config";
 
 
 process.env.OVERRIDE_HUBSPOT_URL = "";
+process.env.CLIENT_ID = "123";
+process.env.CLIENT_SECRET = "123";
 
 const connector = {
   private_settings: {
@@ -72,6 +74,30 @@ it("should send out a new hull account to hubspot insert validation error", () =
         ["debug", "connector.service_api.call", expect.whatever(), expect.whatever()],
         ["debug", "connector.service_api.call", expect.whatever(), expect.whatever()],
         ["debug", "outgoing.job.start", expect.whatever(), {"toInsert": 1, "toSkip": 0, "toUpdate": 0}],
+        [
+          "info",
+          "outgoing.account.skip",
+          {
+            "subject_type": "account",
+            "request_id": expect.whatever(),
+            "account_domain": "hull.io"
+          },
+          {
+            "reason": "There are no outgoing attributes to synchronize for account.  Please go to the settings page and add outgoing account attributes to synchronize"
+          }
+        ],
+        [
+          "info",
+          "outgoing.account.skipcandidate",
+          {
+            "subject_type": "account",
+            "request_id": expect.whatever(),
+            "account_domain": "hull.io"
+          },
+          {
+            "reason": "attribute change not found"
+          }
+        ],
         [
           "debug",
           "connector.service_api.call",
