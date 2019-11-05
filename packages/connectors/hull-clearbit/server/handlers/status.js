fbdbdb263abfad6321b7b38f50431dff1c08bcab
Romain Dardour <romain@hull.io>
2019-11-05 11:11:16 +0100

    improve Status responses

--- packages/connectors/hull-clearbit/server/handlers/status.js
+++ packages/connectors/hull-clearbit/server/handlers/status.js
@@ -9,20 +9,16 @@ export default function statusCheck(ctx: HullContext) {
   let status = "ok";
   const {
     api_key,
-    // discover_enabled,
-    enrich_enabled,
-    prospect_enabled,
     enrich_user_segments,
+    enrich_account_segments,
     reveal_user_segments,
-    reveal_enabled,
     prospect_filter_seniority,
     prospect_filter_titles,
     prospect_filter_role,
     prospect_domain,
-    prospect_segments
+    prospect_account_segments,
+    prospect_limit_count
   } = private_settings;
-
-  const any_enabled = enrich_enabled || prospect_enabled || reveal_enabled;
 
   if (!api_key) {
     status = "warning";
@@ -30,51 +26,61 @@ export default function statusCheck(ctx: HullContext) {
       "No API Key stored, connector is inactive. Enter API key in Settings"
     );
   }
-  if (!any_enabled) {
-    messages.push("No Clearbit API enabled, enable some sections in Settings");
-  }
-  if (enrich_enabled && !_.size(enrich_user_segments)) {
+  if (!_.size(enrich_user_segments)) {
     messages.push(
-      "Enrich enabled, but no segments are listed. No one will be enriched"
+      "Enrich enabled, but no User segments are listed. No User will be enriched"
     );
   }
-  if (reveal_enabled && !_.size(reveal_user_segments)) {
+  if (!_.size(enrich_account_segments)) {
+    messages.push(
+      "Enrich enabled, but no Account segments are listed. No Accounts will be enriched"
+    );
+  }
+  if (!_.size(reveal_user_segments)) {
     messages.push(
       "Reveal enabled, but no segments are listed. No one will be revealed"
     );
   }
 
-  if (prospect_enabled) {
-    if (!_.size(prospect_segments)) {
-      messages.push(
-        "Prospector enabled, but no segments are listed. No one will trigger prospection"
-      );
-    } else if (_.size(prospect_filter_role)) {
-      status = "warning";
-      messages.push(
-        "Prospector enabled, but no Roles are listed. Prospection will be unpredictable"
-      );
-    } else if (!prospect_domain) {
-      status = "error";
-      messages.push(
-        "Prospector enabled, but no 'Company Domain' field is set. We need to know what domain to lookup"
-      );
-    } else if (!prospect_filter_seniority) {
-      status = "ok";
-      messages.push(
-        "Prospector enabled, but no Seniority is listed. Prospection might return underqualified results"
-      );
-    } else if (!prospect_filter_titles) {
-      status = "ok";
-      messages.push(
-        "Prospector enabled, but no Titles are listed. Prospection might return underqualified results"
-      );
-    } else if (!prospect_filter_role) {
-      status = "ok";
-      messages.push(
-        "Prospector enabled, but no Roles are listed. Prospection might return underqualified results"
-      );
-    }
+  if (!_.size(prospect_account_segments)) {
+    messages.push(
+      "Prospector enabled, but no Account segments are listed. No Account will trigger prospection"
+    );
+  }
+  if (_.size(prospect_filter_role)) {
+    status = "warning";
+    messages.push(
+      "Prospector enabled, but no Roles are listed. Prospection will be unpredictable"
+    );
+  }
+  if (!prospect_domain) {
+    status = "error";
+    messages.push(
+      "Prospector enabled, but no 'Company Domain' field is set. We need to know what domain to lookup"
+    );
+  }
+  if (!prospect_filter_seniority) {
+    status = "ok";
+    messages.push(
+      "Prospector enabled, but no Seniority is listed. Prospection might return underqualified results"
+    );
+  }
+  if (!prospect_filter_titles) {
+    status = "ok";
+    messages.push(
+      "Prospector enabled, but no Titles are listed. Prospection might return underqualified results"
+    );
+  }
+  if (!prospect_filter_role) {
+    status = "ok";
+    messages.push(
+      "Prospector enabled, but no Roles are listed. Prospection might return underqualified results"
+    );
+  } else if (prospect_limit_count > 20) {
+    status = "ok";
+    messages.push(
+      `Prospector limit count is high ${prospect_limit_count}. We recommend keeping it under 20`
+    );
   }
   return {
     status,
