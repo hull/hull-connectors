415f634d04140a4377f487d184194c61ce6651c5
Romain Dardour <romain@hull.io>
2019-12-09 18:21:31 +0100

    don't use CurrentUser anymore

--- packages/connectors/hull-browser/src/lib/hull.js
+++ packages/connectors/hull-browser/src/lib/hull.js
@@ -6,12 +6,9 @@ export default function getHullIds() {
   return new Promise(function getId(resolve /* , reject */) {
     const { Hull } = window;
     if (!Hull) return resolve({});
-    return Hull.ready().then(({ hull, me = {} }) => {
+    return Hull.ready().then(({ hull }) => {
       const { anonymousId } = hull.config();
-      if (!me) return resolve({ anonymous_id: anonymousId });
-
-      const { external_id, id, email } = me;
-      return resolve({ id, external_id, anonymous_id: anonymousId, email });
+      return resolve({ anonymous_id: anonymousId });
     });
   });
 }
