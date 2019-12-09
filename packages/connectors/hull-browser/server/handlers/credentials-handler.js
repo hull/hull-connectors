c6ca46c9fd4587c554723bca86b81db5f718ff90
Romain Dardour <romain@hull.io>
2019-12-09 18:24:46 +0100

    udpate Manifest settings

--- packages/connectors/hull-browser/server/handlers/credentials-handler.js
+++ packages/connectors/hull-browser/server/handlers/credentials-handler.js
@@ -14,7 +14,7 @@ const credentialsHandler = (
   return {
     status: 200,
     data: {
-      url: `<script src="https://${hostname}/ship.js" type="text/javascript" async defer data-hull-id="${id}" data-hull-endpoint="https://${hostname}" charset="utf-8"></script>`
+      url: `https://${hostname}/ship.js#${id}`
     }
   };
 };
