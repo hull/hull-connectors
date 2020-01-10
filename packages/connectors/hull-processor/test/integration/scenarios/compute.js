1da04a25d52358c859948440a489dd21e5e962de
Romain Dardour <romain@hull.io>
2020-01-10 11:41:09 +0100

    add a test to prove we don't call alias if alias is already there

--- packages/connectors/hull-processor/test/integration/scenarios/compute.js
+++ packages/connectors/hull-processor/test/integration/scenarios/compute.js
@@ -161,6 +161,44 @@ describe("Basic Attributes manipulation", () => {
     }));
   });
 
+  it("not make any call if user already has alias", () => {
+    const claims = { id: "1234", anonymous_id: "foo" };
+    const asAccount = { id: "1234" };
+    const attributes = { identical: "value" };
+    return testScenario({ connectorConfig }, ({ handlers, nock, expect }) => ({
+      ...messageWithUser({
+        user: {
+          id: "1234",
+          anonymous_ids: ["foo"]
+        },
+        account: {}
+      }),
+      handlerType: handlers.notificationHandler,
+      connector: connectorWithCode(`hull.alias({ anonymous_id: "foo" })`),
+      firehoseEvents: [],
+      logs: [
+        [
+          "debug",
+          "compute.debug",
+          expect.whatever(),
+          expect.objectContaining({
+            userTraits: [],
+            userAliases: [
+              [
+                {
+                  anonymous_id: "foo",
+                  id: "1234"
+                },
+                [[{ anonymous_id: "foo" }, "alias"]]
+              ]
+            ]
+          })
+        ]
+      ],
+      metrics: [METRIC_CONNECTOR_REQUEST]
+    }));
+  });
+
   it("locally omit identical attributes to reduce calls", () => {
     const asUser = {
       id: "1234"
