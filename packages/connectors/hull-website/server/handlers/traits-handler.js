c4d94f51ba3c9bb2d1602fb1452db8ee44b16224
Stephane Bellity <sbellity@gmail.com>
2020-02-27 22:35:34 +0100

    promote email as claim when used in traits call

--- packages/connectors/hull-website/server/handlers/traits-handler.js
+++ packages/connectors/hull-website/server/handlers/traits-handler.js
@@ -1,7 +1,15 @@
 // @flow
 
+const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+
 export default (req, res) => {
   const { name, operation, value } = req.body;
+  const anonymous_id = req.get("hull-bid") || req.cookies._bid;
+
+  if (name === "email" && EMAIL_REGEXP.test(value)) {
+    req.hull = req.hull.asUser({ email: value, anonymous_id });
+  }
+
   return req.hull
     .traits({ [name]: { operation, value } }, req.firehoseEventContext)
     .then(
