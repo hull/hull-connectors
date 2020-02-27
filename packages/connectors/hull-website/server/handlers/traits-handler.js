de350c00471b43d4b561ff2e8aee85d364641cac
Stephane Bellity <sbellity@gmail.com>
2020-02-27 23:05:15 +0100

    Add support for multiple traits in trait payload

--- packages/connectors/hull-website/server/handlers/traits-handler.js
+++ packages/connectors/hull-website/server/handlers/traits-handler.js
@@ -3,15 +3,24 @@
 const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
 
 export default (req, res) => {
-  const { name, operation, value } = req.body;
-  const anonymous_id = req.get("hull-bid") || req.cookies._bid;
+  let traits = req.body;
 
-  if (name === "email" && EMAIL_REGEXP.test(value)) {
-    req.hull = req.hull.asUser({ email: value, anonymous_id });
+  // legacy format sent by hull.js v0.10
+  const { name, operation, value } = traits;
+  if (name && operation && value) {
+    traits =
+      operation === "set"
+        ? { [name]: value }
+        : { [name]: { operation, value } };
+  }
+
+  if (traits.email && EMAIL_REGEXP.test(traits.email)) {
+    const anonymous_id = req.get("hull-bid") || req.cookies._bid;
+    req.hull = req.hull.asUser({ email: traits.email, anonymous_id });
   }
 
   return req.hull
-    .traits({ [name]: { operation, value } }, req.firehoseEventContext)
+    .traits(traits, req.firehoseEventContext)
     .then(
       ok => res.status(204).send({ ok: !!ok }),
       error => res.status(503).send({ error })
