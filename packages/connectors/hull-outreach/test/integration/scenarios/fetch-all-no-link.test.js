cc1611c3954eb9b3f684c8f6206bee416ef0bdbd
Louis Jahn <louis@hull.io>
2020-02-28 14:01:28 +0100

    fix tests

--- packages/connectors/hull-outreach/test/integration/scenarios/fetch-all-no-link.test.js
+++ packages/connectors/hull-outreach/test/integration/scenarios/fetch-all-no-link.test.js
@@ -76,7 +76,7 @@ test("fetch all accounts and prospects from outreach no user/account linking", (
           .post("/api/v2/webhooks/")
           .reply(201, require("../fixtures/api-responses/create-webhook.json"));
         scope
-          .get("/v2/users/?page[limit]=100&filter[id]=0..inf")
+          .get("/api/v2/users/?page[limit]=100&filter[id]=0..inf")
           .reply(201, { data: [ { id: 1, attributes: { email: "andy@hull.io" } }, { id: 0, attributes: { email: "tim@hull.io" }}]});
         // scope
         //   .get("/api/v2/stages/")
