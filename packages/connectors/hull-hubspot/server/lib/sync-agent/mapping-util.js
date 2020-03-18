d65540e3cc595f32ab9542317936f430325eea20
Louis Jahn <louis@hull.io>
2020-03-18 19:10:08 +0100

    Timestamp prefix unnecessary

--- packages/connectors/hull-hubspot/server/lib/sync-agent/mapping-util.js
+++ packages/connectors/hull-hubspot/server/lib/sync-agent/mapping-util.js
@@ -610,7 +610,7 @@ class MappingUtil {
         let value = _.get(userData, mappingEntry.hull_trait_name);
 
         if (
-          (/_(at|date|timestamp)$/.test(mappingEntry.hull_trait_name) ||
+          (/_(at|date)$/.test(mappingEntry.hull_trait_name) ||
             mappingEntry.hubspot_property_type === "datetime") &&
           typeof value === "string"
         ) {
@@ -752,7 +752,7 @@ class MappingUtil {
           : _.get({ account: accountData }, mappingEntry.hull_trait_name);
 
         if (
-          /_(at|date|timestamp)$/.test(mappingEntry.hull_trait_name) ||
+          /_(at|date)$/.test(mappingEntry.hull_trait_name) ||
           mappingEntry.hubspot_property_type === "datetime"
         ) {
           const dateValue = new Date(value).getTime();
