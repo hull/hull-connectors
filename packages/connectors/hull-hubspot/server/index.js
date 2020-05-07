9b2aff3e802544c38953cd7790ca00e39693c0e4
Stephane Bellity <sbellity@gmail.com>
2020-05-07 22:53:00 +0200

    temp handler for uncaughtException

--- packages/connectors/hull-hubspot/server/index.js
+++ packages/connectors/hull-hubspot/server/index.js
@@ -4,3 +4,9 @@ import Hull from "hull";
 import config from "./config";
 
 new Hull.Connector(config).start();
+
+process.on("uncaughtException", function(err) {
+  console.warn("uncaughtException", err);
+  console.error(err.stack);
+  console.log("Node NOT Exiting... !");
+});
