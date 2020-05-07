a41bb4d6c666847f36eba40db2cc67f855072564
Stephane Bellity <sbellity@gmail.com>
2020-05-07 22:55:29 +0200

    use uncaughtExceptionMonitor instead

--- packages/connectors/hull-hubspot/server/index.js
+++ packages/connectors/hull-hubspot/server/index.js
@@ -5,8 +5,10 @@ import config from "./config";
 
 new Hull.Connector(config).start();
 
-process.on("uncaughtException", function(err) {
-  console.warn("uncaughtException", err);
+process.on("uncaughtExceptionMonitor", function(err, origin) {
+  console.error("-------------------------");
+  console.error("uncaughtException :", err);
+  console.error("Exception origin :", origin);
   console.error(err.stack);
-  console.log("Node NOT Exiting... !");
+  console.error("-------------------------");
 });
