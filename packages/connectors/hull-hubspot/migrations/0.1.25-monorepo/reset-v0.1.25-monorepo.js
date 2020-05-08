437404953de69ca2643816929112fa9c2ccfcf31
Andy <andy@hull.io>
2020-05-08 14:26:41 -0400

    migration script update

--- packages/connectors/hull-hubspot/migrations/0.1.25-monorepo/reset-v0.1.25-monorepo.js
+++ packages/connectors/hull-hubspot/migrations/0.1.25-monorepo/reset-v0.1.25-monorepo.js
@@ -3,6 +3,7 @@
 const { Client } = require("hull");
 const superagent = require("superagent");
 const fs = require("fs");
+const _ = require("lodash");
 
 const ships = [
   {
@@ -22,7 +23,7 @@ const parentDirectory = "/Users/hubspot";
       organization: ship.organization
     });
 
-    const fileName = `${ship.organization}.json`;
+    const fileName = `${ship.ship}.${ship.organization}.json`;
 
     console.log("--------------------------------------------------");
     try {
@@ -31,25 +32,39 @@ const parentDirectory = "/Users/hubspot";
         "utf8"
       );
 
-      const oldPrivateSettings = JSON.parse(buf);
-      // eslint-disable-next-line no-await-in-loop
-      await hullClient.utils.settings.update(oldPrivateSettings);
-      console.log("Updated Settings Org:", `${ship.organization}`);
-      // eslint-disable-next-line no-await-in-loop
-      await superagent
-        .agent()
-        .post(
-          `https://${ship.organization}/api/v1/${ship.ship}/update_manifest`
-        )
-        .set("Hull-App-Id", ship.ship)
-        .set("Hull-Access-Token", ship.secret)
-        .then(updateManifestRes => {
-          console.log(
-            "Refresh Manifest:",
-            `org: ${ship.organization}`,
-            `status: ${updateManifestRes.status}`
-          );
-        });
+      hullClient.get("app").then(async connector => {
+        const privateSettings = _.cloneDeep(connector.private_settings);
+
+        const oldPrivateSettings = JSON.parse(buf);
+
+        privateSettings.incoming_user_attributes =
+          oldPrivateSettings.incoming_user_attributes || [];
+        privateSettings.outgoing_user_attributes =
+          oldPrivateSettings.outgoing_user_attributes || [];
+        privateSettings.outgoing_account_attributes =
+          oldPrivateSettings.outgoing_account_attributes || [];
+        privateSettings.incoming_account_attributes =
+          oldPrivateSettings.incoming_account_attributes || [];
+        // eslint-disable-next-line no-await-in-loop
+        await hullClient.utils.settings.update(oldPrivateSettings);
+        console.log("Updated Settings Org:", `${ship.organization}`);
+
+        // eslint-disable-next-line no-await-in-loop
+        await superagent
+          .agent()
+          .post(
+            `https://${ship.organization}/api/v1/${ship.ship}/update_manifest`
+          )
+          .set("Hull-App-Id", ship.ship)
+          .set("Hull-Access-Token", ship.secret)
+          .then(updateManifestRes => {
+            console.log(
+              "Refresh Manifest:",
+              `org: ${ship.organization}`,
+              `status: ${updateManifestRes.status}`
+            );
+          });
+      });
     } catch (err) {
       console.error(
         "ERROR",
