84b7971b1b5b1309000431bb8bd80c8c87d734ca
Louis Jahn <louis@hull.io>
2020-06-05 18:33:35 +0200

    Fix type return in getForms

--- packages/connectors/hull-typeform/server/lib/service-client.js
+++ packages/connectors/hull-typeform/server/lib/service-client.js
@@ -82,7 +82,7 @@ class ServiceClient {
       .set("Authorization", `Bearer ${accessToken}`);
   }
 
-  async getForms(): Array<TypeformFormMinimal> {
+  async getForms(): Promise<Array<TypeformFormMinimal>> {
     let forms = [];
     let page = 1;
     let pageData = await this.agent.get("/forms").query({
