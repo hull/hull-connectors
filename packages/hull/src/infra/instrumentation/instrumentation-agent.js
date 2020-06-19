ff0f0f82f5197daa53973edcef49e9ac2e3c3856
andyh565 <48692643+andyh565@users.noreply.github.com>
2020-06-18 20:42:09 -0400

    new relic start background transaction

--- packages/hull/src/infra/instrumentation/instrumentation-agent.js
+++ packages/hull/src/infra/instrumentation/instrumentation-agent.js
@@ -124,7 +124,7 @@ class InstrumentationAgent {
 
   startTransaction(jobName: string, callback: () => {}) {
     if (this.nr) {
-      return this.nr.createBackgroundTransaction(jobName, callback)();
+      return this.nr.startBackgroundTransaction(jobName, callback)();
     }
     return callback();
   }
