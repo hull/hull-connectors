b58796b28674ec5133522cc230217d752d12828d
Romain Dardour <romain@hull.io>
2020-06-24 16:41:43 +0200

    attempt to sort hash (doesn't work)

--- packages/connectors/hull-dropcontact/server/lib/get-hash.js
+++ packages/connectors/hull-dropcontact/server/lib/get-hash.js
@@ -2,10 +2,21 @@
 
 import crypto from "crypto";
 
-const getHash = (data: {}) => {
-  return crypto
+function getSortedHash(inputHash) {
+  const resultHash = {};
+  Object.keys(inputHash)
+    .sort((a, b) => inputHash[a] - inputHash[b])
+    .reverse()
+    .forEach(k => {
+      resultHash[k] = inputHash[k];
+    });
+  return resultHash;
+}
+
+const getHash = (data: {}) =>
+  crypto
     .createHash("sha384")
-    .update(JSON.stringify(data), "utf-8")
+    .update(JSON.stringify(getSortedHash(data)), "utf-8")
     .digest("hex");
-};
+
 export default getHash;
