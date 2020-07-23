a19efccb9e6b4164895496cf4347c73b1b46953a
Louis Jahn <louis@hull.io>
2020-07-23 17:31:46 +0200

    fix await enqueued job

--- packages/connectors/hull-mailchimp/server/actions/fetch-recent-users.js
+++ packages/connectors/hull-mailchimp/server/actions/fetch-recent-users.js
@@ -5,7 +5,7 @@ import type { HullContext } from "hull";
  *  Trigger a job for importing recently updated contacts in Mailchimp
  */
 async function fetchRecentUsers(ctx: HullContext) {
-  ctx.enqueue("fetchRecentUsers");
+  await ctx.enqueue("fetchRecentUsers");
   return {
     status: 200,
     data: {
