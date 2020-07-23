2b639a2e8308d116fb632e560f3205ac060ea2ec
Romain Dardour <romain@hull.io>
2020-07-23 11:25:52 +0200

    Fix response code for new Phantombuster API

--- packages/connectors/hull-phantombuster/server/handlers/config-data.js
+++ packages/connectors/hull-phantombuster/server/handlers/config-data.js
@@ -5,18 +5,19 @@ import updateAgentDetails from "../lib/agent-details";
 // import type { ConfResponse } from "hull-vm";
 
 const configHandler = async (ctx: HullContext): Promise<Object> => {
-  const { agent, org } = await updateAgentDetails(ctx, true);
-  return {
-    ..._.pick(
-      ctx.connector.private_settings,
-      "agent_id",
-      "org",
-      "api_key",
-      "sync_interval"
-    ),
-    agent,
-    org
-  };
+  const baseconfig = _.pick(
+    ctx.connector.private_settings,
+    "agent_id",
+    "org",
+    "api_key",
+    "sync_interval"
+  );
+  try {
+    const { agent, org } = await updateAgentDetails(ctx, true);
+    return { ...baseconfig, agent, org };
+  } catch (error) {
+    return { ...baseconfig, config_error: error.message };
+  }
 };
 
 export default configHandler;
