153a000ec92047fd246b75d556844c6fee391ced
Louis Jahn <louis@hull.io>
2020-07-28 17:54:59 +0200

    Allow only one concurrent mailchimp for fetch and fetch recent users

--- packages/connectors/hull-mailchimp/server/actions/fetch-recent-users.js
+++ packages/connectors/hull-mailchimp/server/actions/fetch-recent-users.js
@@ -1,15 +1,29 @@
 // @flow
 import type { HullContext } from "hull";
 
+const _ = require("lodash");
+
+const operationName = "fetch_recent_users";
+
 /**
  *  Trigger a job for importing recently updated contacts in Mailchimp
  */
 async function fetchRecentUsers(ctx: HullContext) {
-  ctx.enqueue("fetchRecentUsers");
+  const { connector = {} } = ctx;
+  const { private_settings = {} } = connector;
+  let response;
+  if (_.isNil(private_settings[`${operationName}_batch_id`])) {
+    response = "ok";
+    await ctx.enqueue("fetchRecentUsers", { operationName });
+  } else {
+    response = `Sync operation already running with batch id: ${
+      private_settings[`${operationName}_batch_id`]
+    }. Please wait for the previous one to finish.`;
+  }
   return {
     status: 200,
     data: {
-      status: "ok"
+      response
     }
   };
 }
