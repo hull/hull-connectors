5e91615b0f4dbf5aeaeda76fb4dab93e31514e63
Romain Dardour <romain@hull.io>
2020-08-25 13:32:02 +0200

    lint

--- packages/connectors/hull-intercom/server/config.js
+++ packages/connectors/hull-intercom/server/config.js
@@ -3,7 +3,7 @@
 import type { HullConnectorConfig } from "hull";
 import manifest from "../manifest.json";
 const webhookHandler = require("hull-connector-framework/src/purplefusion/webhooks/webhook-handler");
-const intercomWebhookHandler = require("./incoming-webhook")
+const intercomWebhookHandler = require("./incoming-webhook");
 const _ = require("lodash");
 const HullRouter = require("hull-connector-framework/src/purplefusion/router");
 
@@ -18,6 +18,11 @@ export default function connectorConfig(): HullConnectorConfig {
     OVERRIDE_FIREHOSE_URL,
     SHIP_CACHE_TTL = 60,
     REDIS_URL,
+    COMBINED,
+    SERVER,
+    WORKER,
+    QUEUE_NAME = "queueApp",
+    KUE_PREFIX = "intercom",
     REDIS_MAX_CONNECTIONS = 5,
     REDIS_MIN_CONNECTIONS = 1
   } = process.env;
@@ -29,13 +34,14 @@ export default function connectorConfig(): HullConnectorConfig {
   }
 
   const hostSecret = SECRET || "1234";
-  
+
   return {
     manifest,
     handlers: new HullRouter({
       serviceName: "intercom",
       glue: require("./glue"),
-      services: { intercom:  require("./service")({
+      services: {
+        intercom: require("./service")({
           clientID: CLIENT_ID,
           clientSecret: CLIENT_SECRET
         })
@@ -51,12 +57,12 @@ export default function connectorConfig(): HullConnectorConfig {
     port: PORT || 8082,
     cacheConfig: REDIS_URL
       ? {
-        store: "redis",
-        url: REDIS_URL,
-        ttl: SHIP_CACHE_TTL || 180,
-        max: REDIS_MAX_CONNECTIONS || 5,
-        min: REDIS_MIN_CONNECTIONS || 1
-      }
+          store: "redis",
+          url: REDIS_URL,
+          ttl: SHIP_CACHE_TTL || 180,
+          max: REDIS_MAX_CONNECTIONS || 5,
+          min: REDIS_MIN_CONNECTIONS || 1
+        }
       : undefined,
     middlewares: [],
     serverConfig: {
@@ -88,7 +94,7 @@ export default function connectorConfig(): HullConnectorConfig {
           url: REDIS_URL,
           name: KUE_PREFIX
         }
-      : { store: "memory" }
+      : { store: "memory" },
     rawCustomRoutes: [
       {
         url: "/webhooks",
