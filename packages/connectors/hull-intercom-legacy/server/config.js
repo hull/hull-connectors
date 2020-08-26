0d658040e0d82d379ace89e0f8e47cf46aea237f
Romain Dardour <romain@hull.io>
2020-08-26 10:08:11 +0200

    cleanup legacy intercom

--- packages/connectors/hull-intercom-legacy/server/config.js
+++ packages/connectors/hull-intercom-legacy/server/config.js
@@ -4,27 +4,14 @@ import type { HullConnectorConfig } from "hull";
 import manifest from "../manifest.json";
 import handlers from "./handlers";
 
-const { Queue } = require("hull/src/infra");
-const KueAdapter = require("hull/src/infra/queue/adapter/kue");
-
 export default function connectorConfig(): HullConnectorConfig {
   const {
     SECRET = "1234",
     KUE_PREFIX = "intercom",
     REDIS_URL,
-    QUEUE_NAME = "queueApp",
-    COMBINED,
-    SERVER,
     CLIENT_ID,
-    CLIENT_SECRET,
-    WORKER
+    CLIENT_SECRET
   } = process.env;
-
-  if (COMBINED !== "true" && WORKER !== "true" && SERVER !== "true") {
-    throw new Error(
-      "None of the processes were started, set any of COMBINED, SERVER or WORKER env vars"
-    );
-  }
 
   if (!CLIENT_ID || !CLIENT_SECRET) {
     throw new Error(
@@ -32,28 +19,17 @@ export default function connectorConfig(): HullConnectorConfig {
     );
   }
 
-  const startServer = COMBINED === "true" || SERVER === "true";
-  const startWorker = COMBINED === "true" || WORKER === "true";
-  const hostSecret = SECRET || "1234";
   return {
     manifest,
     handlers: handlers({
-      hostSecret,
+      hostSecret: SECRET || "1234",
       clientID: CLIENT_ID,
       clientSecret: CLIENT_SECRET
     }),
-    serverConfig: {
-      start: startServer
-    },
-    workerConfig: {
-      start: startWorker,
-      queueName: QUEUE_NAME || "queue"
-    },
-    queue: new Queue(
-      new KueAdapter({
-        prefix: KUE_PREFIX,
-        redis: REDIS_URL
-      })
-    )
+    queue: {
+      store: "redis",
+      name: KUE_PREFIX,
+      url: REDIS_URL
+    }
   };
 }
