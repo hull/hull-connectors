7caba720303592940f2560dd79e1d8c0307cc1ba
Romain Dardour <romain@hull.io>
2020-10-23 14:11:51 +0200

    refactor

--- packages/hull/src/utils/merge-config.js
+++ packages/hull/src/utils/merge-config.js
@@ -1,20 +1,18 @@
+import _ from "lodash";
+
 export default function resolveConfig(conf, envConfig) {
   const passedConfig = typeof conf === "function" ? conf() : conf;
-
-  const connectorConfig = {
-    ...passedConfig
-  };
-
-  Object.keys(envConfig).forEach(k => {
-    const val = envConfig[k];
-    if (typeof val === "object") {
-      connectorConfig[k] = {
-        ...(envConfig[k] || {}),
-        ...(passedConfig[k] || {})
-      };
-    } else {
-      connectorConfig[k] = passedConfig[k] || val;
-    }
-  });
-  return connectorConfig;
+  return _.reduce(
+    envConfig,
+    (finalConfig, value, key) => {
+      finalConfig[key] = _.isPlainObject(value)
+        ? {
+            ...value,
+            ..._.get(passedConfig, key, {})
+          }
+        : _.get(passedConfig, key, value);
+      return finalConfig;
+    },
+    passedConfig
+  );
 }
