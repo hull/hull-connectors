ebe2de660d8816f670bdb0b7abfb564f26591a9c
Romain Dardour <romain@hull.io>
2020-11-01 21:15:57 +0100

    parallel-webpack has better performance it seems

--- scripts/build-connectors.sh
+++ scripts/build-connectors.sh
@@ -15,4 +15,4 @@ babel packages/hull-connector-framework -d dist/node_modules/hull-connector-fram
 
 rsync -rl packages/ dist/ --ignore-existing
 
-webpack --progress --config webpack
+parallel-webpack --progress --config webpack
