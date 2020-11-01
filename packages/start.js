d62e1b2a715f698c6ba42a601306f818271ecab6
Romain Dardour <romain@hull.io>
2020-11-01 21:55:38 +0100

    use Env vars to define connector to boot

--- packages/start.js
+++ packages/start.js
@@ -4,23 +4,23 @@ import minimist from "minimist";
 import path from "path";
 
 (async () => {
-  const { connector } = minimist(process.argv);
-  console.log("Start.js, Connector", {
-    args: minimist(process.argv),
-    connector,
-    cwd: process.cwd(),
-    PWD: process.env.PWD,
-    __dirname
-  });
-  const connectorPath = path.join(process.cwd(), connector);
-  console.log("Start.js, ConnectorPath", { connectorPath });
-
-  process.chdir(connectorPath);
-  if (!connector) {
+  const { PATH_TO_CONNECTOR } = process.env;
+  if (!PATH_TO_CONNECTOR) {
     throw new Error(
       "Missing connector - you need to define which connector to start"
     );
   }
+
+  console.log("Start.js, Connector", {
+    args: minimist(process.argv),
+    PATH_TO_CONNECTOR,
+    cwd: process.cwd(),
+    PWD: process.env.PWD,
+    __dirname
+  });
+
+  const connectorPath = path.join(process.cwd(), PATH_TO_CONNECTOR);
+  process.chdir(connectorPath);
 
   try {
     const { default: manifest } = await import(
--- scripts/start-connector.sh
+++ scripts/start-connector.sh
@@ -32,4 +32,4 @@ if [ -n "${MARATHON_APP_ID:-}" ]; then
 fi
 
 echo "Starting connector $PATH_TO_CONNECTOR on PORT $PORT"
-exec pm2 -i max start dist/start.js --no-daemon -- --optimize_for_size --max_old_space_size=$MEMORY_AVAILABLE --gc_interval=100 -r newrelic -r appmetrics/start --connector=$PATH_TO_CONNECTOR
+PATH_TO_CONNECTOR=$PATH_TO_CONNECTOR pm2 -i max start dist/start.js --no-daemon -- --optimize_for_size --max_old_space_size=$MEMORY_AVAILABLE --gc_interval=100 -r newrelic -r appmetrics/start
