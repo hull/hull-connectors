2dfe48804dcf002508deaf4c1db3e3c8fb370889
andyh565 <andy@hull.io>
2020-11-10 12:36:08 -0500

    [hull-hubspot] add manifest to tests

--- packages/connectors/hull-hubspot/test/integration/scenarios/outgoing-user-send-only-changes.js
+++ packages/connectors/hull-hubspot/test/integration/scenarios/outgoing-user-send-only-changes.js
@@ -1,6 +1,7 @@
 // @flow
 
 import connectorConfig from "../../../server/config";
+import manifest from "../../../manifest.json";
 const testScenario = require("hull-connector-framework/src/test-scenario");
 const contactPropertyGroups = [
   ...require("../fixtures/get-contacts-groups"),
@@ -62,7 +63,7 @@ const usersSegments = [
 
 it("should send out a hull user's changes", () => {
   const email = "email@email.com";
-  return testScenario({ connectorConfig }, ({ handlers, nock, expect }) => {
+  return testScenario({ manifest, connectorConfig }, ({ handlers, nock, expect }) => {
     return {
       handlerType: handlers.notificationHandler,
       handlerUrl: "smart-notifier",
@@ -152,7 +153,7 @@ it("should send out a hull user's changes", () => {
 
 it("should create a contact from Hull user with entire attribute mapper", () => {
   const email = "email@email.com";
-  return testScenario({ connectorConfig }, ({ handlers, nock, expect }) => {
+  return testScenario({ manifest, connectorConfig }, ({ handlers, nock, expect }) => {
     return {
       handlerType: handlers.notificationHandler,
       handlerUrl: "smart-notifier",
