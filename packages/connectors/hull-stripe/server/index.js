a59ac49a11e361fff4ad61910cccd42b4a472c80
andyh565 <andy@hull.io>
2020-11-11 14:29:27 -0500

    [hull-stripe] lightweight connector implementation

--- packages/connectors/hull-stripe/server/index.js
+++ packages/connectors/hull-stripe/server/index.js
@@ -1,21 +1,31 @@
 import _ from "lodash";
 import moment from "moment";
 
-export default async function handle({
-  hull,
-  private_settings,
-  headers,
-  body
-}) {
-  // if (body.email) {
-  //   hull.asUser({ email: body.email }).traits(body);
-  // }
-  // hull.logger.info("Foo", { body, private_settings, headers });
+async function handleCustomerData(hull, data) {
+  const userIdent = {
+    email: _.get(data, "email", null),
+    anonymous_id: `stripe:${_.get(data, "id", null)}`
+  };
 
-  /*
-   * Configuration
-   */
-  const typeMappingsEvents = [{
+  const attribs = {
+    "stripe/email": _.get(data, "email", null),
+    "stripe/id": _.get(data, "id", null),
+    "stripe/created_at": moment(
+      _.get(data, "created", null) * 1000
+    ).toISOString(),
+    "stripe/delinquent": _.get(data, "delinquent", null),
+    "stripe/discount": _.get(data, "discount", null),
+    "stripe/account_balance": _.get(data, "balance", null),
+    "stripe/description": _.get(data, "description", null),
+    "stripe/currency": _.get(data, "currency", null)
+  };
+
+  return hull.asUser(userIdent).traits(attribs);
+}
+
+export default async function handle({ hull, body }) {
+  const typeMappingsEvents = [
+    {
       key: "charge.failed",
       name: "Charge failed"
     },
@@ -58,9 +68,8 @@ export default async function handle({
     {
       key: "customer.subscription.deleted",
       name: "Subscription Ended"
-    },
+    }
   ];
-
 
   /*
    * General Execution
@@ -71,15 +80,18 @@ export default async function handle({
   const objId = _.get(body, "id", null);
   const payloadType = _.get(body, "type", null);
 
-  console.log(liveMode, payloadType, objType);
   if (liveMode !== true) {
-    return;
+    return {};
   }
 
-  if (payloadType === "customer.updated" || payloadType === "customer.created") {
+  if (
+    payloadType === "customer.updated" ||
+    payloadType === "customer.created"
+  ) {
     console.log("Customer created or updated");
-    handleCustomerData(data.object);
-  } else if (objType === "event") {
+    return handleCustomerData(hull, data.object);
+  }
+  if (objType === "event") {
     const currentType = _.find(typeMappingsEvents, {
       key: payloadType
     });
@@ -96,11 +108,11 @@ export default async function handle({
         event_id: objId,
         created_at
       }
-    }
+    };
     hull.logger.debug(currentType);
 
     if (!currentType) {
-      return;
+      return {};
     }
 
     _.set(trackEvent, "eventName", currentType.name);
@@ -109,6 +121,7 @@ export default async function handle({
       case "charge.failed":
       case "charge.succeeeded":
       case "charge.refunded":
+        // eslint-disable-next-line no-case-declarations
         const mapCharge = {
           id: "charge_id?",
           invoice: "invoice_id?",
@@ -127,17 +140,32 @@ export default async function handle({
         };
 
         _.forIn(mapCharge, (v, k) => {
-          if (_.endsWith(v.replace("?", ""), "_at") && !_.isNil(_.get(data.object, k, null))) {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, moment(_.get(data.object, k) * 1000).toISOString());
+          if (
+            _.endsWith(v.replace("?", ""), "_at") &&
+            !_.isNil(_.get(data.object, k, null))
+          ) {
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              moment(_.get(data.object, k) * 1000).toISOString()
+            );
           } else {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, _.get(data.object, k, null));
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              _.get(data.object, k, null)
+            );
           }
         });
 
         // User Ident
         _.set(userIdent, "email", _.get(data.object, "customer_email", null));
         if (!_.isNil(_.get(data.object, "customer", null))) {
-          _.set(userIdent, "anonymous_id", `stripe:${_.get(data.object, "customer", null)}`);
+          _.set(
+            userIdent,
+            "anonymous_id",
+            `stripe:${_.get(data.object, "customer", null)}`
+          );
         }
         break;
       case "invoice.payment_failed":
@@ -145,6 +173,7 @@ export default async function handle({
       case "invoice.upcoming":
       case "invoice.updated":
       case "invoice.created":
+        // eslint-disable-next-line no-case-declarations
         const mapInvoice = {
           tax: "tax",
           tax_percent: "tax_percent",
@@ -179,34 +208,70 @@ export default async function handle({
           "discount.coupon.duration": "coupon_duration",
           "discount.coupon.duration_in_months": "coupon_duration_in_months",
           "discount.coupon.percent_off": "coupon_percent_off",
-          amount_refunded: "amount_refunded",
+          amount_refunded: "amount_refunded"
         };
 
         _.forIn(mapInvoice, (v, k) => {
-          if (_.endsWith(v.replace("?", ""), "_at") && !_.isNil(_.get(data.object, k, null))) {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, moment(_.get(data.object, k) * 1000).toISOString());
+          if (
+            _.endsWith(v.replace("?", ""), "_at") &&
+            !_.isNil(_.get(data.object, k, null))
+          ) {
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              moment(_.get(data.object, k) * 1000).toISOString()
+            );
           } else {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, _.get(data.object, k, null));
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              _.get(data.object, k, null)
+            );
           }
         });
 
         // Summarize line data
-        _.set(trackEvent, "props.amounts", _.get(data.object, "lines.data", []).map(l => l.amount));
-        _.set(trackEvent, "props.plan_ids", _.get(data.object, "lines.data", []).map(l => _.get(l, "plan.id", null)));
-        _.set(trackEvent, "props.plan_names", _.get(data.object, "lines.data", []).map(l => _.get(l, "plan.name", null)));
-        _.set(trackEvent, "props.plan_amounts", _.get(data.object, "lines.data", []).map(l => _.get(l, "plan.amount", null)));
+        _.set(
+          trackEvent,
+          "props.amounts",
+          _.get(data.object, "lines.data", []).map(l => l.amount)
+        );
+        _.set(
+          trackEvent,
+          "props.plan_ids",
+          _.get(data.object, "lines.data", []).map(l =>
+            _.get(l, "plan.id", null)
+          )
+        );
+        _.set(
+          trackEvent,
+          "props.plan_names",
+          _.get(data.object, "lines.data", []).map(l =>
+            _.get(l, "plan.name", null)
+          )
+        );
+        _.set(
+          trackEvent,
+          "props.plan_amounts",
+          _.get(data.object, "lines.data", []).map(l =>
+            _.get(l, "plan.amount", null)
+          )
+        );
 
         // User Ident
         _.set(userIdent, "email", _.get(data.object, "customer_email", null));
         if (!_.isNil(_.get(data.object, "customer", null))) {
-          _.set(userIdent, "anonymous_id", `stripe:${_.get(data.object, "customer", null)}`);
+          _.set(
+            userIdent,
+            "anonymous_id",
+            `stripe:${_.get(data.object, "customer", null)}`
+          );
         }
-
 
         break;
       case "customer.subscription.created":
       case "customer.subscription.updated":
-      case "customer.subscription.updated":
+        // eslint-disable-next-line no-case-declarations
         const mapSubscription = {
           id: "subscription_id?",
           "items.total_count": "items_count?",
@@ -229,80 +294,45 @@ export default async function handle({
           trial_start: "trial_start_at?"
         };
         _.forIn(mapSubscription, (v, k) => {
-          if (_.endsWith(v.replace("?", ""), "_at") && !_.isNil(_.get(data.object, k, null))) {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, moment(_.get(data.object, k) * 1000).toISOString());
+          if (
+            _.endsWith(v.replace("?", ""), "_at") &&
+            !_.isNil(_.get(data.object, k, null))
+          ) {
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              moment(_.get(data.object, k) * 1000).toISOString()
+            );
           } else {
-            _.set(trackEvent, `props.${v.replace("?", "")}`, _.get(data.object, k, null));
+            _.set(
+              trackEvent,
+              `props.${v.replace("?", "")}`,
+              _.get(data.object, k, null)
+            );
           }
         });
 
         // User Ident
         _.set(userIdent, "email", _.get(data.object, "customer_email", null));
         if (!_.isNil(_.get(data.object, "customer", null))) {
-          _.set(userIdent, "anonymous_id", `stripe:${_.get(data.object, "customer", null)}`);
+          _.set(
+            userIdent,
+            "anonymous_id",
+            `stripe:${_.get(data.object, "customer", null)}`
+          );
         }
 
         break;
+      default:
+        console.log("missed conditions");
     }
-
 
     userIdent = _.pickBy(userIdent, _.identity);
     if (Object.keys(userIdent).length > 0) {
-      hull.asUser(userIdent)
+      return hull
+        .asUser(userIdent)
         .track(trackEvent.eventName, trackEvent.props, trackEvent.ctx);
     }
-
   }
-
-
-
-  /*
-   * Utility functions
-   */
-  function flatten(data) {
-    var result = {};
-
-    function recurse(cur, prop) {
-      if (Object(cur) !== cur) {
-        result[prop] = cur;
-      } else if (Array.isArray(cur)) {
-        for (var i = 0, l = cur.length; i < l; i++)
-          recurse(cur[i], prop + "[" + i + "]");
-        if (l == 0)
-          result[prop] = [];
-      } else {
-        var isEmpty = true;
-        for (var p in cur) {
-          isEmpty = false;
-          recurse(cur[p], prop ? prop + "__" + p : p);
-        }
-        if (isEmpty && prop)
-          result[prop] = {};
-      }
-    }
-    recurse(data, "");
-    return result;
-  }
-
-  function handleCustomerData(data) {
-    const userIdent = {
-      email: _.get(data, "email", null),
-      anonymous_id: `stripe:${_.get(data, "id", null)}`
-    };
-
-    const attribs = {
-      "stripe/email": _.get(data, "email", null),
-      "stripe/id": _.get(data, "id", null),
-      "stripe/created_at": moment(_.get(data, "created", null) * 1000).toISOString(),
-      "stripe/delinquent": _.get(data, "delinquent", null),
-      "stripe/discount": _.get(data, "discount", null),
-      "stripe/account_balance": _.get(data, "balance", null),
-      "stripe/description": _.get(data, "description", null),
-      "stripe/currency": _.get(data, "currency", null),
-    };
-
-    hull.asUser(userIdent).traits(attribs);
-    // console.log(userIdent, attribs);
-  }
-
+  return {};
 }
