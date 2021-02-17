58a90bf5b5e39a2dbdba5b4c95d191984ad17b97
Romain Dardour <romain@hull.io>
2021-02-17 13:24:14 +0100

    re-enable

--- packages/connectors/hull-outgoing-account-webhooks/test/integration/outgoing-account-trigger.test.js
+++ packages/connectors/hull-outgoing-account-webhooks/test/integration/outgoing-account-trigger.test.js
@@ -90,243 +90,243 @@ describe("Validate IP Ranges", () => {
     ));
 });
 
-// describe("Outgoing Account Tests", () => {
-//   it("Account Created. Should Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "is_new_account"
-//         });
+describe("Outgoing Account Tests", () => {
+  it("Account Created. Should Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "is_new_account"
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {
-//             const scope = nock(VALID_URL);
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {
+            const scope = nock(VALID_URL);
 
-//             scope
-//               .post("/mock", {
-//                 domain: "apple.com"
-//               })
-//               .reply(200, {
-//                 status: 200
-//               });
+            scope
+              .post("/mock", {
+                domain: "apple.com"
+              })
+              .reply(200, {
+                status: 200
+              });
 
-//             return scope;
-//           },
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+            return scope;
+          },
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Account Entered Segment. Should Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_segments_entered"
-//         });
+  it("Account Entered Segment. Should Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_segments_entered"
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {
-//             const scope = nock(VALID_URL);
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {
+            const scope = nock(VALID_URL);
 
-//             scope
-//               .post("/mock", {
-//                 domain: "apple.com"
-//               })
-//               .reply(200, {
-//                 status: 200
-//               });
+            scope
+              .post("/mock", {
+                domain: "apple.com"
+              })
+              .reply(200, {
+                status: 200
+              });
 
-//             return scope;
-//           },
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+            return scope;
+          },
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Account Left Segment. Should Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_segments_left"
-//         });
+  it("Account Left Segment. Should Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_segments_left"
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {
-//             const scope = nock(VALID_URL);
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {
+            const scope = nock(VALID_URL);
 
-//             scope
-//               .post("/mock", {
-//                 domain: "apple.com"
-//               })
-//               .reply(200, {
-//                 status: 200
-//               });
+            scope
+              .post("/mock", {
+                domain: "apple.com"
+              })
+              .reply(200, {
+                status: 200
+              });
 
-//             return scope;
-//           },
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+            return scope;
+          },
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Whitelisted Account Attribute Changed. Should Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_attribute_updated"
-//         });
+  it("Whitelisted Account Attribute Changed. Should Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_attribute_updated"
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {
-//             const scope = nock(VALID_URL);
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {
+            const scope = nock(VALID_URL);
 
-//             scope
-//               .post("/mock", {
-//                 domain: "apple.com"
-//               })
-//               .reply(200, {
-//                 status: 200
-//               });
+            scope
+              .post("/mock", {
+                domain: "apple.com"
+              })
+              .reply(200, {
+                status: 200
+              });
 
-//             return scope;
-//           },
-//           response: triggerScenario.getExpectedResponse(),
+            return scope;
+          },
+          response: triggerScenario.getExpectedResponse(),
 
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   // NEGATIVES
+  // NEGATIVES
 
-//   it("Account in whitelisted segment with no other trigger defined. Should Not Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_synchronized_segment",
-//           negative: true
-//         });
+  it("Account in whitelisted segment with no other trigger defined. Should Not Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_synchronized_segment",
+          negative: true
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {},
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {},
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Account Entered Segment. Should Not Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_segments_entered",
-//           negative: true
-//         });
+  it("Account Entered Segment. Should Not Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_segments_entered",
+          negative: true
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {},
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {},
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Account Left Segment. Should Not Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_segments_left",
-//           negative: true
-//         });
+  it("Account Left Segment. Should Not Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_segments_left",
+          negative: true
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {},
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {},
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
 
-//   it("Whitelisted Account Attribute Changed. Should Not Send Payload", () => {
-//     return testScenario(
-//       { manifest, connectorConfig },
-//       ({ handlers, nock, expect }) => {
-//         const triggerScenario = createSimpleTriggerScenario({
-//           trigger: "account_attribute_updated",
-//           negative: true
-//         });
+  it("Whitelisted Account Attribute Changed. Should Not Send Payload", () => {
+    return testScenario(
+      { manifest, connectorConfig },
+      ({ handlers, nock, expect }) => {
+        const triggerScenario = createSimpleTriggerScenario({
+          trigger: "account_attribute_updated",
+          negative: true
+        });
 
-//         return _.assign(triggerScenario.getScenarioDefinition(), {
-//           handlerType: handlers.notificationHandler,
-//           handlerUrl: "smart-notifier",
-//           channel: "account:update",
-//           externalApiMock: () => {},
-//           response: triggerScenario.getExpectedResponse(),
-//           logs: triggerScenario.getExpectedLogs(),
-//           firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
-//           metrics: triggerScenario.getExpectedMetrics(),
-//           platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
-//         });
-//       }
-//     );
-//   });
-// });
+        return _.assign(triggerScenario.getScenarioDefinition(), {
+          handlerType: handlers.notificationHandler,
+          handlerUrl: "smart-notifier",
+          channel: "account:update",
+          externalApiMock: () => {},
+          response: triggerScenario.getExpectedResponse(),
+          logs: triggerScenario.getExpectedLogs(),
+          firehoseEvents: triggerScenario.getExpectedFirehoseEvents(),
+          metrics: triggerScenario.getExpectedMetrics(),
+          platformApiCalls: triggerScenario.getExpectedPlatformApiCalls()
+        });
+      }
+    );
+  });
+});
